79d7a58522d3b0b2c82eed004091cd7a96c562e9
mike0041@gmail.com <mike0041@gmail.com@1fd3dd75-cea3-417f-6d24-55f5e259fd08>
2011-09-12 14:16:58 +0000

    Cleanup the prototype implementation of the AreaLight

--- trunk/yaphyre/src/yaphyre/lights/AreaLight.java
+++ trunk/yaphyre/src/yaphyre/lights/AreaLight.java
@@ -2,6 +2,8 @@ package yaphyre.lights;
 
 import java.text.MessageFormat;
 
+import yaphyre.geometry.Matrix;
+import yaphyre.geometry.TransformationMatrixBuilder;
 import yaphyre.geometry.Vector;
 import yaphyre.raytracer.CollisionInformations;
 import yaphyre.raytracer.Scene;
@@ -21,9 +23,9 @@ public class AreaLight extends AbstractLightsource {
 
   private final Vector orientation;
 
-  private final double width;
+  private final double size;
 
-  private final int numberOfSamples;
+  private final int samplesPerSide;
 
   private final int numberOfRays;
 
@@ -31,34 +33,27 @@ public class AreaLight extends AbstractLightsource {
 
   private final double intensity;
 
-  private Vector[] originPoints;
-
-  public AreaLight(String id, Vector position, Vector orientation, double width, int numberOfSamples, double intensity, Color color, Falloff falloff) {
+  public AreaLight(String id, Vector position, Vector orientation, double size, int samplesPerSide, double intensity, Color color, Falloff falloff) {
     super(id, position, color, falloff);
     this.orientation = orientation;
-    this.width = width;
-    this.numberOfSamples = numberOfSamples;
-    this.numberOfRays = this.numberOfSamples * this.numberOfSamples;
+    this.size = size;
+    this.samplesPerSide = samplesPerSide;
+    this.numberOfRays = this.samplesPerSide * this.samplesPerSide;
     this.intensity = intensity;
     this.rayIntensity = this.intensity / this.numberOfRays;
-    this.originPoints = null;
   }
 
   @Override
   public String toString() {
-    return MessageFormat.format(TO_STRING_FORMAT, getId(), getPosition(), this.orientation, this.width, getColor(), this.numberOfSamples, this.intensity);
+    return MessageFormat.format(TO_STRING_FORMAT, getId(), getPosition(), this.orientation, this.size, getColor(), this.samplesPerSide, this.intensity);
   }
 
   @Override
   public double getIntensity(Vector point, Scene scene) {
 
-    if (originPoints == null) {
-      this.initializeOrigins();
-    }
-
     double accumulatedIntensity = 0d;
 
-    for (Vector origin : originPoints) {
+    for (Vector origin : createOrigins()) {
       CollisionInformations shadowCollision = super.calculateVisibility(origin, point, scene);
       if (shadowCollision == null) {
         accumulatedIntensity += super.getFalloff().getIntensity(this.rayIntensity, origin.sub(point).length());
@@ -69,15 +64,29 @@ public class AreaLight extends AbstractLightsource {
 
   }
 
-  private void initializeOrigins() {
-    this.originPoints = new Vector[this.numberOfRays];
-    double dx = this.width / this.numberOfSamples;
-    double dy = this.width / this.numberOfSamples;
-    for (int yIndex = 0; yIndex < this.numberOfSamples; yIndex++) {
-      for (int xIndex = 0; xIndex < this.numberOfSamples; xIndex++) {
-        int index = yIndex * this.numberOfSamples + xIndex;
+  /**
+   * Calculates an array of origin points based on the distribution in a 1x1
+   * unit rectangle which is the transformed into the scene coordinate space by
+   * the lights position and direction.
+   * 
+   * @return An array of {@link Vector}, each representing a start point for a
+   *         light ray.
+   */
+  private Vector[] createOrigins() {
+    Vector[] originPoints = new Vector[this.numberOfRays];
+    Matrix tranformationMatrix = TransformationMatrixBuilder.matrix().forTranslation(getPosition()).build();
+    double dx = 1d / this.samplesPerSide;
+    double dy = 1d / this.samplesPerSide;
+    double startx = -0.5d;
+    double starty = -0.5d;
+    for (int yIndex = 0; yIndex < this.samplesPerSide; yIndex++) {
+      for (int xIndex = 0; xIndex < this.samplesPerSide; xIndex++) {
+        int index = yIndex * this.samplesPerSide + xIndex;
+        Vector localStartPoint = new Vector(startx + xIndex * dx, starty + yIndex * dy, 0);
+        originPoints[index] = localStartPoint;
       }
     }
+    return originPoints;
   }
 
 }
