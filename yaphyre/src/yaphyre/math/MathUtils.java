38f06c40773193a7028c6cb2349a14579a9d383c
mike0041@gmail.com <mike0041@gmail.com@1fd3dd75-cea3-417f-6d24-55f5e259fd08>
2011-09-14 20:05:27 +0000

    Equation solver added

--- yaphyre/src/yaphyre/math/MathUtils.java
+++ yaphyre/src/yaphyre/math/MathUtils.java
@@ -2,12 +2,32 @@ package yaphyre.math;
 
 public class MathUtils {
 
+  private static final double LARGE_VAL = 1e100;
+
   public static double max(double v1, double v2, double v3) {
     return Math.max(v1, Math.max(v2, v3));
   }
 
   public static double min(double v1, double v2, double v3) {
     return Math.min(v1, Math.min(v2, v3));
+  }
+
+  public static double div(double a, double b) {
+    if (a == 0) {
+      return 0;
+    }
+    if (b == 0) {
+      return LARGE_VAL * sign(a);
+    } else {
+      if ((a + b) == a) {
+        return LARGE_VAL * sign(a) * sign(b);
+      }
+      return a / b;
+    }
+  }
+
+  public static double sign(double x) {
+    return (x == 0) ? 0 : (x > 0) ? 1 : -1;
   }
 
 }
