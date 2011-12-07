22d08400c48bdc34a9d4af0b1004aa3770a91c5c
mike0041@gmail.com <mike0041@gmail.com@1fd3dd75-cea3-417f-6d24-55f5e259fd08>
2011-12-07 12:21:59 +0000

    Matrix and Transformation cleanup

--- yaphyre/src/yaphyre/geometry/Matrix.java
+++ yaphyre/src/yaphyre/geometry/Matrix.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 Michael Bieri
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -29,9 +29,9 @@ import org.apache.commons.math.linear.RealMatrix;
 
 /**
  * Rudimentary implementation of some essential matrix operations.
- * 
+ *
  * @version $Revision$
- * 
+ *
  * @author Michael Bieri
  * @author $LastChangedBy$
  */
@@ -183,7 +183,6 @@ public class Matrix {
   }
 
   private void calculateInternals() {
-    Matrix inverseMatrix = null;
     try {
       RealMatrix rm = MatrixUtils.createRealMatrix(this.m);
       LUDecomposition decomp = new LUDecompositionImpl(rm);
