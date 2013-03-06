f906733910b93e942f000e470057f7c0acebf2a8
mike0041@gmail.com <mike0041@gmail.com@1fd3dd75-cea3-417f-6d24-55f5e259fd08>
2013-03-06 21:19:29 +0000

    Collision detection fixed

--- trunk/yaphyre/src/yaphyre/raytracer/Scene.java
+++ trunk/yaphyre/src/yaphyre/raytracer/Scene.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Michael Bieri
+ * Copyright 2013 Michael Bieri
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -83,9 +83,10 @@ public class Scene implements Serializable {
 		CollisionInformation result = null;
 
 		for (Shape shape : getShapes()) {
-			result = shape.intersect(ray);
-			if (result != null && result.getDistance() < nearestCollisionDistance) {
-				nearestCollisionDistance = result.getDistance();
+			CollisionInformation collisionInformation = shape.intersect(ray);
+			if (collisionInformation != null && collisionInformation.getDistance() < nearestCollisionDistance) {
+				nearestCollisionDistance = collisionInformation.getDistance();
+				result = collisionInformation;
 			}
 		}
 
