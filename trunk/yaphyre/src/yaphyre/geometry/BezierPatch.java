96ed8f05bd910a94b97a2a7a21262dbb460043cd
mike0041@gmail.com <mike0041@gmail.com@1fd3dd75-cea3-417f-6d24-55f5e259fd08>
2013-03-13 16:00:24 +0000

    Initial structure for BezierPatch calculation

--- trunk/yaphyre/src/yaphyre/geometry/BezierPatch.java
+++ trunk/yaphyre/src/yaphyre/geometry/BezierPatch.java
@@ -22,7 +22,23 @@ import static org.apache.commons.math.util.MathUtils.binomialCoefficientDouble;
 import org.jetbrains.annotations.NotNull;
 
 /**
- *
+ * Calculates a single point on a bezier patch. The format of the data is a two dimensional array.
+ * Example:
+ * <pre>
+ *     p[0][2] p[1][2] p[2][2] p[3][2]
+ *     p[0][1] p[1][1] p[2][1] p[3][1]
+ *     p[0][0] p[1][0] p[2][0] p[3][0]
+ * </pre>
+ * The u bezier-functions contain the following control points:
+ * <pre>
+ *     B0 = p[0][0], p[1][0], p[2][0], p[3][0]
+ *     B1 = p[0][1], p[1][1], p[2][1], p[3][1]
+ *     B2 = p[0][2], p[1][2], p[2][2], p[3][2]
+ * </pre>
+ * From these u bezier-functions, the corresponding v control points are calculated by evaluating each
+ * set of control points for the given u-value. After that, the calculated control points are used as control
+ * points for a bezier curve in the v-direction which is evaluated for the given v-value. The result is the
+ * point in question.
  */
 public enum BezierPatch {
 	GENERIC {
