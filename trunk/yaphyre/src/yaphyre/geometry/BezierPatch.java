d51e89119a8b26f8d0bb9f28ad51eb7e85fdbd32
mike0041@gmail.com <mike0041@gmail.com@1fd3dd75-cea3-417f-6d24-55f5e259fd08>
2013-03-19 15:47:05 +0000

    BezierPatch reworked

--- trunk/yaphyre/src/yaphyre/geometry/BezierPatch.java
+++ trunk/yaphyre/src/yaphyre/geometry/BezierPatch.java
@@ -19,25 +19,7 @@ package yaphyre.geometry;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Calculates a single point on a bezier patch. The format of the data is a two dimensional array. Example:
- * <pre>
- *     p[0][2] p[1][2] p[2][2] p[3][2]
- *     p[0][1] p[1][1] p[2][1] p[3][1]
- *     p[0][0] p[1][0] p[2][0] p[3][0]
- * </pre>
- * The u bezier-functions contain the following control points:
- * <pre>
- *     Bu0 = p[0][0], p[1][0], p[2][0], p[3][0]
- *     Bu1 = p[0][1], p[1][1], p[2][1], p[3][1]
- *     Bu2 = p[0][2], p[1][2], p[2][2], p[3][2]
- * </pre>
- * The v bezier-function correspondingly contain the following control points:
- * <pre>
- *     Bv0 = p[0][0], p[0][1], p[0][2]
- *     Bv1 = p[1][0], p[1][1], p[1][2]
- *     Bv2 = p[2][0], p[2][1], p[2][2]
- *     Bv3 = p[3][0], p[3][1], p[3][2]
- * </pre>
+ * Calculates a single point on a bezier patch. The format of the data is a linear array to make the usage simpler.
  * From these u bezier-functions, the corresponding v control points are calculated by evaluating each set of control
  * points for the given u-value. After that, the calculated control points are used as control points for a bezier
  * curve in the v-direction which is evaluated for the given v-value. The result is the point in question.
@@ -46,10 +28,8 @@ public enum BezierPatch {
 	GENERIC {
 		@NotNull
 		@Override
-		public Point3D calculateMeshPoint(final double u, final double v, @NotNull final Point3D[][] controlPoints) {
-
-			final int uOrder = controlPoints.length;
-			final int vOrder = controlPoints[0].length;
+		public Point3D calculateMeshPoint(final double u, final double v, final int uOrder, final int vOrder,
+				final int startIndex, @NotNull final Point3D... controlPoints) {
 
 			final Point3D[] vControlPoints = new Point3D[vOrder];
 
@@ -57,7 +37,7 @@ public enum BezierPatch {
 			for (int vIndex = 0; vIndex < vOrder; vIndex++) {
 				final Point3D[] uControlPoints = new Point3D[uOrder];
 				for (int uIndex = 0; uIndex < uOrder; uIndex++) {
-					uControlPoints[uIndex] = controlPoints[uIndex][vIndex];
+					uControlPoints[uIndex] = controlPoints[MathUtils.calculateIndex(startIndex, uIndex, vIndex)];
 				}
 				vControlPoints[vIndex] = BezierCurve.GENERIC.calculatePoint(u, uControlPoints);
 			}
@@ -65,9 +45,11 @@ public enum BezierPatch {
 			// evaluate the new bezier curve for the calculated control points for v
 			return BezierCurve.GENERIC.calculatePoint(v, vControlPoints);
 		}
+
 	};
 
 	@NotNull
-	public abstract Point3D calculateMeshPoint(double u, double v, @NotNull Point3D[][] controlPoints);
+	public abstract Point3D calculateMeshPoint(final double u, final double v, final int uOrder, final int vOrder,
+			final int startIndex, @NotNull final Point3D... controlPoints);
 
 }
