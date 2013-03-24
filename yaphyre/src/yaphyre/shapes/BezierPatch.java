ef7a469df3c99150ba304418c25e99bfdf4b6105
mike0041@gmail.com <mike0041@gmail.com@1fd3dd75-cea3-417f-6d24-55f5e259fd08>
2013-03-24 12:24:53 +0000

    BezierPatch tessellation implemented

--- yaphyre/src/yaphyre/shapes/BezierPatch.java
+++ yaphyre/src/yaphyre/shapes/BezierPatch.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright 2013 Michael Bieri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package yaphyre.shapes;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static org.apache.commons.math.util.FastMath.min;
 
 import yaphyre.core.BoundingBox;
 import yaphyre.core.CollisionInformation;
@@ -20,8 +37,8 @@ import org.jetbrains.annotations.Nullable;
  */
 public class BezierPatch extends AbstractShape {
 
-	private static final double U_RESOLUTION = 0.001;
-	private static final double V_RESOLUTION = U_RESOLUTION;
+	private static final int U_RESOLUTION = 10;
+	private static final int V_RESOLUTION = U_RESOLUTION;
 
 	private final int uDimension;
 	private final int vDimension;
@@ -72,14 +89,52 @@ public class BezierPatch extends AbstractShape {
 		return null;
 	}
 
-	private void tessellateMesh() {
+	private void tessellateMesh(final int uResolution, final int vResolution) {
+		Point3D[] patchPoints = calculateMeshPoints(uResolution, vResolution);
+		int[][] triangles = createTriangleIndices(uResolution, vResolution);
+		tessellatedPatch = new TriangleMesh(Transformation.IDENTITY, getShader(), patchPoints, triangles);
 		state = TessellationState.Tessellated;
+	}
+
+	Point3D[] calculateMeshPoints(final int uResolution, final int vResolution) {
+		final int arraySize = (uResolution + 1) * (vResolution + 1);
+		final double uStepSize = 1d / uResolution;
+		final double vStepSize = 1d / vResolution;
+		Point3D[] patchPoints = new Point3D[arraySize];
+		for(int uStep = 0; uStep <= uResolution; uStep++) {
+			for(int vStep = 0; vStep <= vResolution; vStep++) {
+				final double u = min(1d, uStep * uStepSize);
+				final double v = min(1d, vStep * vStepSize);
+				patchPoints[(vStep + uStep * vDimension)] = yaphyre.geometry.BezierPatch.GENERIC.calculateMeshPoint(u, v, uDimension, vDimension, 0, controlPoints);
+			}
+		}
+		return patchPoints;
+	}
+
+	int[][] createTriangleIndices(final int uResolution, final int vResolution) {
+		int[][] triangles = new int[uResolution * vResolution * 2][3];
+		final int vStripSize = vResolution + 1;
+		for(int uStep = 0, triangleIndex = 0; uStep < uResolution; uStep++) {
+			for(int vStep = 0; vStep < vResolution; vStep++) {
+				triangles[triangleIndex] = new int[3];
+				triangles[triangleIndex][0] = vStep + uStep * vStripSize;
+				triangles[triangleIndex][1] = vStep + (uStep + 1) * vStripSize;
+				triangles[triangleIndex][2] = vStep + 1 + (uStep + 1) * vStripSize;
+				triangleIndex++;
+				triangles[triangleIndex] = new int[3];
+				triangles[triangleIndex][0] = vStep + uStep * vStripSize;
+				triangles[triangleIndex][1] = vStep + 1 + (uStep + 1) * vStripSize;
+				triangles[triangleIndex][2] = vStep + 1 + uStep * vStripSize;
+				triangleIndex++;
+			}
+		}
+		return triangles;
 	}
 
 	private void tessellateIfNecessary() {
 		switch (state) {
 		case NotTessellated:
-			tessellateMesh();
+			tessellateMesh(U_RESOLUTION, V_RESOLUTION);
 			break;
 		case Failed:
 			throw new IllegalStateException("Failed to tessellate bezier patch: " + toString());
