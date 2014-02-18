e7678e5db12ac01f268c7bb97c8f4e44ac1e2605
mike0041@gmail.com <mike0041@gmail.com@1fd3dd75-cea3-417f-6d24-55f5e259fd08>
2014-02-18 09:16:23 +0000

    Orthographic camera implementation

--- yaphyre/src/test/java/yaphyre/cameras/OrthographicCameraTest.java
+++ yaphyre/src/test/java/yaphyre/cameras/OrthographicCameraTest.java
@@ -18,23 +18,28 @@ package yaphyre.cameras;
 
 import org.apache.commons.math3.util.Pair;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import yaphyre.core.CameraSample;
 import yaphyre.core.Film;
+import yaphyre.core.Scene;
+import yaphyre.core.Tracer;
+import yaphyre.math.Color;
 import yaphyre.math.Point2D;
 import yaphyre.math.Ray;
 import yaphyre.math.Vector3D;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static yaphyre.math.MathUtils.EPSILON;
 
 /**
  * YaPhyRe
  *
- * @author mike0041@gmail.com
+ * @author Michael Bieri
  * @since 18.02.14
  */
 public class OrthographicCameraTest {
@@ -42,32 +47,50 @@ public class OrthographicCameraTest {
     /** Dimensions used for all tests. */
     private static final int DIMENSION = 10;
 
+    /** Size of the film in x dimension. */
+    public static final int X_SIZE = 4;
+
+    /** Size of the film in y dimension. */
+    public static final int Y_SIZE = 3;
+
     /** Instance for all test methods. */
     private OrthographicCamera testCamera;
+
+    /** Mocked tracer instance for verification. */
+    private Tracer tracer;
+
+    /** Mocked film instance for verification. */
+    private Film film;
 
     /**
      * Setup the object under test.
      */
     @Before
     public void setupTestCamera() {
-        Film film = mock(Film.class);
-        when(film.getNativeResolution()).thenReturn(new Pair<>(4, 3));
+        film = mock(Film.class);
+        when(film.getNativeResolution()).thenReturn(new Pair<>(X_SIZE, Y_SIZE));
+        tracer = mock(Tracer.class);
+        when(tracer.traceRay(any(Ray.class), any(Scene.class))).thenReturn(Color.BLACK);
         testCamera = new OrthographicCamera<>(film, DIMENSION, DIMENSION);
+        testCamera.setTracer(tracer);
     }
 
     @Test
     public void testCreateCameraRay() throws Exception {
+        // corner case [0, 0]
         Ray cameraRay = testCamera.createCameraRay(new Point2D(0d, 0d));
         assertEquals(-DIMENSION / 2d, cameraRay.getOrigin().getX(), EPSILON);
         assertEquals(-DIMENSION / 2d, cameraRay.getOrigin().getY(), EPSILON);
         assertEquals(0d, cameraRay.getOrigin().getZ(), EPSILON);
         assertEquals(cameraRay.getDirection(), Vector3D.Z.neg());
 
+        // corner case [1, 1]
         cameraRay = testCamera.createCameraRay(new Point2D(1d, 1d));
         assertEquals(DIMENSION / 2d, cameraRay.getOrigin().getX(), EPSILON);
         assertEquals(DIMENSION / 2d, cameraRay.getOrigin().getY(), EPSILON);
         assertEquals(0d, cameraRay.getOrigin().getZ(), EPSILON);
 
+        // case [0.5, 0.5] --> [0, 0, 0]
         cameraRay = testCamera.createCameraRay(new Point2D(0.5d, 0.5d));
         assertEquals(0d, cameraRay.getOrigin().getX(), EPSILON);
         assertEquals(0d, cameraRay.getOrigin().getY(), EPSILON);
@@ -75,8 +98,10 @@ public class OrthographicCameraTest {
     }
 
     @Test
-    @Ignore
     public void testRenderScene() throws Exception {
-        fail("Not implemented yet");
+        Scene scene = mock(Scene.class);
+        testCamera.renderScene(scene);
+        verify(tracer, times(X_SIZE * Y_SIZE)).traceRay(any(Ray.class), any(Scene.class));
+        verify(film, times(X_SIZE * Y_SIZE)).addCameraSample(any(CameraSample.class));
     }
 }
